b79cd645f2639a484a4a92edcb40616e17551741
Angelo Esam <angloesam61@gmail.com>
2025-04-23 02:43:46 +0200

    finish

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,38 @@ import Register from "../pages/Register.jsx";
 import Login from "../pages/Login.jsx";
 import Home from "../pages/Home.jsx";
 import Header from "../components/Header.jsx";
+import ProtectedRoute from "../utils/ProtectedRoute.jsx";
+import PublicRoute from "../utils/PublicRoute.jsx";
 
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
       </Routes>
     </Router>
   );
---
+++ frontend/utils/PublicRoute.jsx
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+
+const PublicRoute = ({ children }) => {
+  const user = localStorage.getItem("token");
+
+  if (user) {
+    return <Navigate to="/" />; 
+  }
+
+  return children;
+};
+
+export default PublicRoute;
